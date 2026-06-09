4a6446937943d83c6e9131111af346cd49ee431b
fardhanrasya <fardhanhafist14@gmail.com>
2026-06-09 22:10:32 +0700

    update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://warkopyaman.com',
+  site: 'https://warkop-yaman.vercel.app',
   integrations: [sitemap()]
 });
